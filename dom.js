d9c4ea2ecb6c91569b242e2c0dd0be81d9df00b8
Ramroop26 <ramroop26io071@satiengg.in>
2025-05-26 00:37:46 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,129 @@
+// DOM => DOCUMENT OBJECT MODEL
+
+// 1. document.getElementById()
+// 2. document.getElementByClass()
+// 3. document.querySelector(" # / . ")
+
+//let callme=()=>{
+    // window.alert("hello world")
+    //document.write("hello world")
+//     let mytag = document.querySelector("#text")
+//     let hel = document.querySelector(".hello")
+//      hel.innerHTML="<h1> hello world<h1/>"
+//     mytag.innerHTML="<i>Welcome Ramroop Prajapati<i/>"
+//     mytag.style.color="red"
+//     mytag.style.backgroundColor="green"
+    
+
+// }
+// let hello=()=>{
+//     let hel = document.querySelector(".hello")
+//     hel.innerHTML="<b> hello world<b/>"
+
+// }
+//----------------------------------------------------------------------------------//-----------------------------------------------/
+// let  col = document.querySelector("#text")
+// let re=()=>{
+    
+//     col.innerHTML="<h1>WELCOME RAMROOP PRAJAPATI<h1/>"
+//     col.style.color="blue"
+//     col.style.backgroundColor="red"
+// }
+
+// let gr=()=>{
+  
+//     col.innerHTML="<h1>WELCOME RAMROOP PRAJAPATI<h1/>"
+//     col.style.color="yellow"
+//     col.style.backgroundColor="green"
+// }
+// let ye=()=>{
+   
+//     col.innerHTML="<h1>WELCOME RAMROOP PRAJAPATI<h1/>"
+//     col.style.color="red"
+//     col.style.backgroundColor="yellow"
+// }
+//--------------------------------------------------------------------------------BOX------------------------------------------------------//
+
+// let orange = ()=>{
+//     let show = document.querySelector("#box")
+//     let btn1 = document.querySelector(".btn1")
+//     show.style.backgroundColor="orange"
+//     show.style.border="5px solid blue"
+//     btn1.style.backgroundColor="red"
+// }
+
+// let green = ()=>{
+//     let show1= document.querySelector("#box")
+//     let btn2 = document.querySelector(".btn2")
+//     show1.style.backgroundColor="green"
+//     show1.style.border="5px solid yellow"
+//     btn2.style.backgroundColor="yellow"
+// }
+
+// let reset = ()=>{
+//     let show2= document.querySelector("#box")
+//     let btn3 = document.querySelector(".btn3")
+//     show2.style.backgroundColor=""
+//     show2.style.border=""
+//     btn3.style.backgroundColor="green"
+// }
+
+//-------------------------------------------------------------------------IMAGE--------------------------------------------------//
+// let orange = ()=>{
+//     let show = document.querySelector("#box")
+//     let btn1 = document.querySelector(".btn1")
+//     show.style.border="5px solid blue"
+//     btn1.style.backgroundColor="red"
+//     show.style.backgroundImage="URL(https://tse3.mm.bing.net/th?id=OIP.U_VJuupQohwnzXcKMztqWgHaEo&pid=Api&P=0&h=180)"
+// }
+
+// let green = ()=>{
+//     let show1= document.querySelector("#box")
+//     let btn2 = document.querySelector(".btn2")
+//     show1.style.backgroundImage="URL(https://tse3.mm.bing.net/th?id=OIP.j4KdqaXpnhbN94WzVyHUhAHaE8&pid=Api&P=0&h=180)"  
+//     show1.style.border="5px solid yellow"
+//     btn2.style.backgroundColor="yellow"
+// }
+
+// let reset = ()=>{
+//     let show2= document.querySelector("#box")
+//     let btn3 = document.querySelector(".btn3")
+//     show2.style.backgroundColor=""
+//     show2.style.border=""
+//     btn3.style.backgroundColor="green"
+//     show2.style.backgroundImage=""
+// }
+
+//-------------------------------------------------------------------Change Image---------------------------------------------------------//
+// let changeImage=()=>{
+//     let Image = document.querySelector("#imagg")
+//     Image.src="https://tse3.mm.bing.net/th?id=OIP.U_VJuupQohwnzXcKMztqWgHaEo&pid=Api&P=0&h=180"
+// }
+// //--------------------------------------------------------------------------- hide text-----------------------------------------------//
+let hide=()=> {
+    let hide = document.querySelector("#text")
+    if( hide.style.display == "none"){
+        hide.style.display = "block";
+        hide.style.color = "green";
+    }
+    else{
+        hide.style.display = "none";
+
+    }
+}
+
+/* The code you provided is defining three functions: */
+// let  hide = ()=>{
+//     let show  = document.querySelector("#text")
+//     show.style.display= "none";
+// }
+
+// let  show = ()=>{
+//     let show  = document.querySelector("#text")
+//     show.style.display = "block";
+// }
+
+// let  reset = ()=>{
+//     let show  = document.querySelector("#text")
+//     show.style.fontSize = "";
+// }
---
+++ loop.js
@@ -0,0 +1,24 @@
+//FOR OF LOOP
+// let arr = ["MERN","PYTHON","JAVA","PHP"]
+// for(let i of arr){
+//     console.log(i)
+// }
+
+//FOR IN LOOP
+// let obj = {
+//     first:"RAMROOP",
+//     last:"PRAJAPATI",
+//     city:"Bhopal",
+//     age:22
+// }
+// for(let i in obj){
+//     console.log(i)
+// }
+
+let num=[4,2,5,6,7]
+let newnum = num.map((e)=>{
+    return e*e
+})
+console.log(newnum);
+//------------------------------------------------------------CALL BACK FUNCTION-------------------------------//
+//Call back function
